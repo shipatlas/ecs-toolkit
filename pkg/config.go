97e04174c9c405cc4217d884c1fa3618dba3a3e9
King'ori Maina <j@kingori.co>
2022-11-16 23:07:11 +0200

    Add service containers to application configuration

--- pkg/config.go
+++ pkg/config.go
@@ -2,19 +2,21 @@ package pkg
 
 type Config struct {
 	Version     *string      `mapstructure:"version" validate:"required"`
-	Application *Application `mapstructure:"application" validate:"required"`
+	Application *Application `mapstructure:"application" validate:"required,dive"`
 }
 
 type Application struct {
 	Cluster  *string    `mapstructure:"cluster" validate:"required"`
-	Services []*Service `mapstructure:"services"`
-	Tasks    []*Task    `mapstructure:"tasks"`
+	Services []*Service `mapstructure:"services" validate:"dive"`
+	Tasks    []*Task    `mapstructure:"tasks" validate:"dive"`
 }
 
 type Service struct {
-	Name *string `mapstructure:"name" validate:"required"`
+	Name       *string   `mapstructure:"name" validate:"required"`
+	Containers []*string `mapstructure:"containers" validate:"required,min=1,dive"`
 }
 
 type Task struct {
-	Name *string `mapstructure:"name" validate:"required"`
+	Name       *string   `mapstructure:"name" validate:"required"`
+	Containers []*string `mapstructure:"containers" validate:"required,min=1,dive"`
 }
