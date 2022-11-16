6e78a0c129d142789d51d492d0acd270d90386d2
King'ori Maina <j@kingori.co>
2022-11-16 23:16:00 +0200

    Add --image-tag flag to deploy command

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -5,20 +5,44 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type deployOptions struct {
+	imageTag string
+}
+
 var (
 	deployCmdLong = utils.LongDesc(`
 		Deploy an application to AWS ECS`)
+
+	deployCmdExamples = utils.Examples(`
+		# Deploy new revision of an application
+		ecs-toolkit deploy --image-tag=5a853f72`)
+
+	deployCmdOptions = &deployOptions{}
 )
 
 // deployCmd represents the deploy command
 var deployCmd = &cobra.Command{
-	Use:   "deploy",
-	Short: "Deploy an application to AWS ECS.",
-	Long:  deployCmdLong,
+	Use:     "deploy",
+	Short:   "Deploy an application to AWS ECS.",
+	Long:    deployCmdLong,
+	Example: deployCmdExamples,
 	Run: func(cmd *cobra.Command, args []string) {
+		deployCmdOptions.validate()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(deployCmd)
+
+	// Local flags, which, will be global for the application.
+	deployCmd.Flags().StringVarP(&deployCmdOptions.imageTag, "image-tag", "t", "", "image tag to update the container images to")
+
+	// Configure required flags, applying to this specific command.
+	deployCmd.MarkFlagRequired("image-tag")
+}
+
+func (options *deployOptions) validate() {
+	if options.imageTag == "" {
+		log.Fatal().Msg("image-tag flag must be set and should not be blank")
+	}
 }
