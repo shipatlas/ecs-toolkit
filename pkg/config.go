a75ec01e596a1b28ba62d36fd65a59d7726ea201
King'ori Maina <j@kingori.co>
2022-11-17 16:46:03 +0200

    Add force configuration to application configuration

--- pkg/config.go
+++ pkg/config.go
@@ -14,9 +14,11 @@ type Application struct {
 type Service struct {
 	Name       *string   `mapstructure:"name" validate:"required"`
 	Containers []*string `mapstructure:"containers" validate:"required,min=1,dive"`
+	Force      *bool     `mapstructure:"force"`
 }
 
 type Task struct {
 	Name       *string   `mapstructure:"name" validate:"required"`
 	Containers []*string `mapstructure:"containers" validate:"required,min=1,dive"`
+	Force      *bool     `mapstructure:"force"`
 }
