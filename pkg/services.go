44e7bdec9a8da3db5e2aa140fc1c983201c50929
King'ori Maina <j@kingori.co>
2022-12-06 11:28:41 +0300

    Refactor services code

--- pkg/services.go
+++ pkg/services.go
@@ -7,190 +7,159 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func (config *Config) DeployServices(newContainerImageTag *string, client *ecs.Client) {
-	clusterSublogger := log.WithFields(log.Fields{
-		"cluster": *config.Cluster,
-	})
+	clusterSublogger := log.WithFields(log.Fields{"cluster": *config.Cluster})
 	clusterSublogger.Info("starting rollout to services")
-
-	// Prepare service mapping for easy lookup later, basically create a map
-	// with the service name as the key and the service as the value in the
-	// services list as the value.
-	serviceMapping := make(map[string]Service)
-	for _, service := range config.Services {
-		serviceMapping[*service.Name] = *service
-	}
 
 	// Get list of services to update from the config file but do not proceed if
 	// there are no services to update.
-	configServiceNames := config.ServiceNames()
-	if len(configServiceNames) == 0 {
+	if len(config.Services) == 0 {
 		clusterSublogger.Warn("skipping rollout to services, none found")
 
 		return
 	}
 
-	// Fetch full profiles of the services so that later we can reference their
-	// attributes e.g. their task definitions.
-	clusterSublogger.Infof("fetching service profiles: %s", strings.Join(configServiceNames, ", "))
-	servicesParams := &ecs.DescribeServicesInput{
-		Cluster:  config.Cluster,
-		Services: configServiceNames,
+	// Process each service on its own asynchronously to reduce the amount of
+	// time spent rolling them out. We should update each service at the same
+	// time.
+	wg := sync.WaitGroup{}
+	wg.Add(len(config.Services))
+	for _, serviceConfig := range config.Services {
+		go deployService(config.Cluster, serviceConfig, newContainerImageTag, client, clusterSublogger, &wg)
 	}
-	servicesResult, err := client.DescribeServices(context.TODO(), servicesParams)
+	wg.Wait()
+
+	clusterSublogger.Info("completed rollout to services")
+}
+
+func deployService(cluster *string, serviceConfig *Service, newContainerImageTag *string, client *ecs.Client, logger *log.Entry, deployWg *sync.WaitGroup) {
+	defer deployWg.Done()
+
+	// Set up new logger with the service name.
+	serviceSublogger := logger.WithField("service", *serviceConfig.Name)
+
+	// Fetch full profile of the service so that later we can reference its
+	// attributes i.e. task definitions.
+	serviceSublogger.Info("fetching service profile")
+	serviceParams := &ecs.DescribeServicesInput{
+		Cluster:  cluster,
+		Services: []string{*serviceConfig.Name},
+	}
+	serviceResult, err := client.DescribeServices(context.TODO(), serviceParams)
 	if err != nil {
-		clusterSublogger.Fatalf("unable to fetch service profiles: %v", err)
+		serviceSublogger.Fatalf("unable to fetch service profile: %v", err)
 	}
 
-	// It's not guaranteed that all the services listed exist on the cluster so
-	// generate a new list of service names of the ones that do.
-	clusterServiceNames := []string{}
-	for _, service := range servicesResult.Services {
-		clusterServiceNames = append(clusterServiceNames, *service.ServiceName)
+	// If the service is not found then stop deploying to the service. We should
+	// also only ever receive one service.
+	if len(serviceResult.Services) == 0 {
+		serviceSublogger.Info("stopped deploy, service not found")
+	}
+	service := serviceResult.Services[0]
+
+	// Store information on which containers should be updated.
+	taskContainerUpdateable := make(map[string]bool)
+	for _, containerName := range serviceConfig.Containers {
+		taskContainerUpdateable[*containerName] = true
 	}
 
-	// If there are no services in the cluster then we should bail out.
-	if len(clusterServiceNames) == 0 {
-		clusterSublogger.Fatal("unable to proceed with deployment, services not found in the cluster")
+	// Generate new task definition with the required changes.
+	taskDefinitionInput := GenerateTaskDefinitionInput{
+		ImageTag:             newContainerImageTag,
+		TaskDefinition:       service.TaskDefinition,
+		UpdateableContainers: taskContainerUpdateable,
+	}
+	newTaskDefinition, taskDefinitionUpdated := GenerateTaskDefinition(&taskDefinitionInput, client, serviceSublogger)
+
+	// Prepare parameters for service.
+	updateServiceParams := &ecs.UpdateServiceInput{
+		Service:                       service.ServiceName,
+		CapacityProviderStrategy:      service.CapacityProviderStrategy,
+		Cluster:                       service.ClusterArn,
+		DeploymentConfiguration:       service.DeploymentConfiguration,
+		DesiredCount:                  &service.DesiredCount,
+		EnableECSManagedTags:          &service.EnableECSManagedTags,
+		EnableExecuteCommand:          &service.EnableECSManagedTags,
+		ForceNewDeployment:            *serviceConfig.Force,
+		HealthCheckGracePeriodSeconds: service.HealthCheckGracePeriodSeconds,
+		LoadBalancers:                 service.LoadBalancers,
+		NetworkConfiguration:          service.NetworkConfiguration,
+		PlacementConstraints:          service.PlacementConstraints,
+		PlacementStrategy:             service.PlacementStrategy,
+		PlatformVersion:               service.PlatformVersion,
+		PropagateTags:                 service.PropagateTags,
+		ServiceRegistries:             service.ServiceRegistries,
 	}
 
-	// Raise warning if there's a mismatch in the services in the config and
-	// those found in the cluster. Make sure we update DescribeServicesInput
-	// just in case it's ever used further down.
-	if len(configServiceNames) > len(clusterServiceNames) {
-		servicesParams.Services = clusterServiceNames
-		clusterSublogger.Warnf("some services missing, limiting to: %s", strings.Join(clusterServiceNames, ", "))
+	// Set task definition.
+	if taskDefinitionUpdated {
+		serviceSublogger.Info("updated task definition, using new one")
+		updateServiceParams.TaskDefinition = newTaskDefinition.TaskDefinitionArn
+	} else {
+		serviceSublogger.Info("no changes to previous task definition, using latest")
+		updateServiceParams.TaskDefinition = serviceConfig.Name
 	}
 
-	// Loop through all services, fetch the latest task definition, make a new
-	// revision of it with updated image tags and finally update the service to
-	// use the new revision.
-	updatedServiceNames := []*string{}
-	for _, service := range servicesResult.Services {
-		serviceSublogger := log.WithFields(log.Fields{
-			"cluster": *config.Cluster,
-			"service": *service.ServiceName,
-		})
-
-		// Store information on which containers should be updated.
-		serviceContainerUpdateable := make(map[string]bool)
-		for _, containerName := range serviceMapping[*service.ServiceName].Containers {
-			serviceContainerUpdateable[*containerName] = true
-		}
-
-		// Generate new task definition with the required changes.
-		taskDefinitionInput := GenerateTaskDefinitionInput{
-			ImageTag:             newContainerImageTag,
-			TaskDefinition:       service.TaskDefinition,
-			UpdateableContainers: serviceContainerUpdateable,
-		}
-		newTaskDefinition, taskDefinitionUpdated := GenerateTaskDefinition(&taskDefinitionInput, client, serviceSublogger)
-
-		// Prepare parameters for task
-		updateServiceParams := &ecs.UpdateServiceInput{
-			Service:                       service.ServiceName,
-			CapacityProviderStrategy:      service.CapacityProviderStrategy,
-			Cluster:                       service.ClusterArn,
-			DeploymentConfiguration:       service.DeploymentConfiguration,
-			DesiredCount:                  &service.DesiredCount,
-			EnableECSManagedTags:          &service.EnableECSManagedTags,
-			EnableExecuteCommand:          &service.EnableECSManagedTags,
-			ForceNewDeployment:            *serviceMapping[*service.ServiceName].Force,
-			HealthCheckGracePeriodSeconds: service.HealthCheckGracePeriodSeconds,
-			LoadBalancers:                 service.LoadBalancers,
-			NetworkConfiguration:          service.NetworkConfiguration,
-			PlacementConstraints:          service.PlacementConstraints,
-			PlacementStrategy:             service.PlacementStrategy,
-			PlatformVersion:               service.PlatformVersion,
-			PropagateTags:                 service.PropagateTags,
-			ServiceRegistries:             service.ServiceRegistries,
-			TaskDefinition:                newTaskDefinition.TaskDefinitionArn,
-		}
-
-		// Set task definition
-		if taskDefinitionUpdated {
-			serviceSublogger.Info("changes made, using new task definition")
-			updateServiceParams.TaskDefinition = newTaskDefinition.TaskDefinitionArn
-		} else {
-			serviceSublogger.Info("no changes, using latest task definition")
-			updateServiceParams.TaskDefinition = service.TaskDefinition
-		}
-
-		serviceSublogger.Info("attempting to update service")
-		_, err = client.UpdateService(context.TODO(), updateServiceParams)
-		if err != nil {
-			clusterSublogger.Fatalf("unable to update service: %v", err)
-		}
-
-		updatedServiceNames = append(updatedServiceNames, service.ServiceName)
-		serviceSublogger.Info("successfully updated service")
+	// Update service to reflect changes.
+	serviceSublogger.Info("attempting to update service")
+	_, err = client.UpdateService(context.TODO(), updateServiceParams)
+	if err != nil {
+		serviceSublogger.Fatalf("unable to update service: %v", err)
 	}
+	serviceSublogger.Info("updated service successfully")
 
-	if len(updatedServiceNames) == 0 {
-		clusterSublogger.Info("completed rollout to services")
-
-		return
-	}
-
-	// Follow rollout of all services until all have a final status.
-	clusterSublogger.Info("watch rollout progress of services")
-	WatchServicesDeployment(config.Cluster, updatedServiceNames, client)
+	// Watch each service deployment until all have a final status.
+	serviceSublogger.Info("watch rollout progress of services")
+	watchService(cluster, &service, client, serviceSublogger)
 
 	// Make sure we wait for rollout of all services.
-	clusterSublogger.Info("checking if all services are stable")
+	serviceSublogger.Info("checking if all services are stable")
 	waiter := ecs.NewServicesStableWaiter(client)
 	maxWaitTime := 15 * time.Minute
-	err = waiter.Wait(context.TODO(), servicesParams, maxWaitTime, func(o *ecs.ServicesStableWaiterOptions) {
+	err = waiter.Wait(context.TODO(), serviceParams, maxWaitTime, func(o *ecs.ServicesStableWaiterOptions) {
 		o.MinDelay = 5 * time.Second
 		o.MaxDelay = 120 * time.Second
 		o.LogWaitAttempts = log.IsLevelEnabled(log.DebugLevel) || log.IsLevelEnabled(log.TraceLevel)
 	})
 	if err != nil {
-		clusterSublogger.Fatalf("unable to check if all services are stable: %v", err)
+		serviceSublogger.Fatalf("unable to check if all services are stable: %v", err)
 
 	}
-	clusterSublogger.Info("completed rollout to services")
 }
 
-func WatchServicesDeployment(cluster *string, serviceNames []*string, client *ecs.Client) {
-	wg := sync.WaitGroup{}
-	wg.Add(len(serviceNames))
-	for _, serviceName := range serviceNames {
-		go watchServiceDeployment(cluster, serviceName, client, &wg)
-	}
-	wg.Wait()
-}
-
-func watchServiceDeployment(cluster *string, serviceName *string, client *ecs.Client, wg *sync.WaitGroup) {
-	defer wg.Done()
-
+func watchService(cluster *string, service *types.Service, client *ecs.Client, serviceSublogger *log.Entry) {
 	ticker := time.NewTicker(time.Second * 3).C
 
 	for {
 		serviceParams := &ecs.DescribeServicesInput{
 			Cluster:  cluster,
-			Services: []string{*serviceName},
+			Services: []string{*service.ServiceName},
 		}
 		serviceResult, err := client.DescribeServices(context.TODO(), serviceParams)
 		if err != nil {
-			log.WithFields(log.Fields{
-				"cluster": *cluster,
-			}).Fatalf("unable to fetch service profile: %v", err)
+			serviceSublogger.Fatalf("unable to fetch service profile: %v", err)
 		}
 
-		// If the service is not found then stop watching the service. We should
+		// If the service is not found then stop deploying to the service. We should
 		// also only ever receive one service.
-		if len(serviceResult.Services) != 1 {
+		if len(serviceResult.Services) == 0 {
+			serviceSublogger.Info("stopped deploy, service not found")
+
 			break
 		}
+		service := serviceResult.Services[0]
 
 		activeService := false
-		service := serviceResult.Services[0]
 		for _, deployment := range service.Deployments {
+			// Set up logger with the deployment identifier.
+			deploymentSublogger := serviceSublogger.WithField("deployment-id", deployment.Id)
+			deploymentSublogger.Infof("watching ... service: %s, deployment: %s, rollout: %d/%d (%d pending)", strings.ToLower(*service.Status), strings.ToLower(*deployment.Status), deployment.RunningCount, deployment.DesiredCount, deployment.PendingCount)
+
 			// PRIMARY The most recent deployment of a service. ACTIVE A service
 			// deployment that still has running tasks, but are in the process
 			// of being replaced with a new PRIMARY deployment. INACTIVE A
@@ -201,14 +170,13 @@ func watchServiceDeployment(cluster *string, serviceName *string, client *ecs.Cl
 			if *deployment.Status == "ACTIVE" {
 				activeService = true
 			}
-
-			log.WithFields(log.Fields{
-				"cluster": *cluster,
-				"service": *serviceName,
-			}).Infof("watching ... service: %s, deployment: %s, rollout: %d/%d (%d pending)", strings.ToLower(*service.Status), strings.ToLower(*deployment.Status), deployment.RunningCount, deployment.DesiredCount, deployment.PendingCount)
 		}
 
-		if !activeService {
+		// If the service is active then there's no need to watch it any
+		// longer.
+		if activeService {
+			serviceSublogger.Infof("service is active")
+
 			break
 		}
 
